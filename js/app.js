8d934231fe7b371a8e3eef550c2b36001717f75e
DavidXploidz <david.adame.itd@gmail.com>
2023-06-26 12:25:21 -0600

    thanks suscribe agregado

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@
 const form_input_email = document.querySelector('#email');
 const form_btn_submit = document.querySelector('.form__btn-submit');
 const form_mensaje = document.querySelector('.form__mensaje');
+const mensaje_success = document.querySelector('#mensaje-success');
+const card_form = document.querySelector('#card-form');
+const card_success = document.querySelector('#card-success');
+const dismiss = document.querySelector('#dismiss');
 
 form_input_email.addEventListener('change', (e) => {
     // console.log(e.target.value);
@@ -23,6 +27,7 @@ const validarEmail = (email) => {
         return;
     }
     imprimirMensaje('OK', 'success');
+    suscrito(email);
 }
 
 const imprimirMensaje = (mensaje, tipo) => {
@@ -37,3 +42,16 @@ const imprimirMensaje = (mensaje, tipo) => {
         form_input_email.classList.add('form__input--success');
     }
 }
+
+const suscrito = (email) => {
+    card_form.classList.add('card--none');
+    card_success.classList.add('card__success--block');
+    mensaje_success.textContent = email;
+}
+
+dismiss.addEventListener('click', () => {
+    form_input_email.value = '';
+    form_input_email.classList.remove('form__input--success');
+    card_form.classList.remove('card--none');
+    card_success.classList.remove('card__success--block');
+});
